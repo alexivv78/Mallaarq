a49a7a93cac9dc23b2aa0c31786ba768a984f56b
alexivv78 <karol.zunigav@gmail.com>
2025-07-15 20:23:10 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,12 @@
-
+document.querySelectorAll('.ramo').forEach((ramo) => {
+  ramo.addEventListener('click', () => {
+    if (!ramo.classList.contains('activado')) {
+      ramo.classList.add('activado');
+      const next = ramo.dataset.next?.split(" ") || [];
+      next.forEach(id => {
+        const nextRamo = document.querySelector(`[data-id="${id}"]`);
+        if (nextRamo) nextRamo.classList.add('activado');
+      });
+    }
+  });
+});
